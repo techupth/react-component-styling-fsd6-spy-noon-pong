2b2fffc85f8340284fe4e74d017c428a0a82e2bf
Pong <pongpong34756@gmail.com>
2024-06-04 14:49:14 +0700

    feat: component hw

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,80 @@
-// Start coding here
+// /** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+function Alert(props) {
+  if (props.type === "error") {
+    return (
+      <>
+        <alert
+          css={css`
+            padding: 0px 16px;
+            font-size: 20px;
+            height: 76px;
+            width: 650px;
+            border-radius: 10px;
+            cursor: pointer;
+            background-color: #f9c8c8;
+          `}
+        >
+          This is error alert box
+        </alert>
+      </>
+    );
+  } else if (props.type === "warning") {
+    return (
+      <>
+        <alert
+          css={css`
+            padding: 0px 16px;
+            font-size: 20px;
+            height: 76px;
+            width: 650px;
+            border-radius: 10px;
+            cursor: pointer;
+            background-color: #f9d9c8;
+          `}
+        >
+          This is warning alert box
+        </alert>
+      </>
+    );
+  } else if (props.type === "info") {
+    return (
+      <>
+        <alert
+          css={css`
+            padding: 0px 16px;
+            font-size: 20px;
+            height: 76px;
+            width: 650px;
+            border-radius: 10px;
+            cursor: pointer;
+            background-color: #f9ebc8;
+          `}
+        >
+          This is info alert box
+        </alert>
+      </>
+    );
+  } else if (props.type === "success") {
+    return (
+      <>
+        <alert
+          css={css`
+            padding: 0px 16px;
+            font-size: 20px;
+            height: 76px;
+            width: 650px;
+            border-radius: 10px;
+            cursor: pointer;
+            background-color: #cef7cd;
+          `}
+        >
+          This is success alert box
+        </alert>
+      </>
+    );
+  }
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,44 @@
-// Start coding here
+// /** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <>
+        <button
+          css={css`
+            padding: 0px 16px;
+            font-size: 24px;
+            height: 50px;
+            width: 172px;
+            border-radius: 5px;
+            cursor: pointer;
+            background-color: #074ee8;
+          `}
+        >
+          primary button
+        </button>
+      </>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <>
+        <button
+          css={css`
+            padding: 0px 16px;
+            font-size: 24px;
+            height: 50px;
+            width: 172px;
+            border-radius: 5px;
+            cursor: pointer;
+            background-color: #07a4e8;
+          `}
+        >
+          secondary button
+        </button>
+      </>
+    );
+  }
+}
+
+export default Button;
